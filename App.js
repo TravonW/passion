f024b840c4a1478725ed9ce3f0de3795e2be00d6
TXT-57 <travonwilliams321@gmail.com>
2023-07-07 09:53:56 -0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Image } from "react-native";
+import Lucki from "./assets/images/mom.jpeg"
+import styles from "./assets/styles/styles";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View >
+      <View style={styles.ImageContainer}>
+      <Image style={styles.Image}source={Lucki } />
+      <View styles={styles.textContainer}>
+      <Text style={styles.words}> Role </Text>
+      <Text styles={styles.text}> Im a Lead Developer</Text>
+      <Text styles={styles.words}> Passion </Text>
+      <Text styles={styles.text}> I am very passionate about game design and i want to practice my code , so i can become a software engieer to get insights about upcoming tech and videogames, also hopefully get early access</Text>
+      <Text styles={styles.words}> Dream Career</Text>
+      <Text styles={styles.text}> I wish to be a software engineer or Hr at a game desiging company to have insights or as an Hr to be able to cheer people up</Text>
+      </View>
+       </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ assets/styles/styles.js
@@ -0,0 +1,42 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    Image: {
+        width: 200,
+        height: 200,
+        alignSelf: "center"
+        
+
+        
+    },
+    ImageContainer:{
+        maringTop: 200,
+      
+
+        
+
+    },
+    words:{
+        textAlign: "center",
+        fontSize: 30,
+        color: "red",
+        backgroundColor: "greed"
+        
+
+    },
+    textContainer:{
+        marginTop: 50,
+        
+
+    },
+    text:{
+        color: "white",
+        textAlign: "center",
+        marginTop: 30,
+        paddingBottom: 50,
+        backgroundColor: "green"
+
+
+    }
+
+})
